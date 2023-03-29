d9988ca3886bb633f5f00feb04cc17f4af6e4812
Benjie-San <haisesasaki56@gmail.com>
2023-03-29 17:13:03 +0800

    added data and expandable component

--- Screens/Brp.js
+++ Screens/Brp.js
@@ -1,12 +1,89 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, ScrollView, LayoutAnimation, Image } from 'react-native'
+import React, {useState, useEffect} from 'react';
 
-export default function Brp({navigation}) {
+const data = require("../constants/data.json");
+
+let content = Object.keys(data).map( (key, index) => 
+    (
+        {
+            isExpanded: false,
+            category_name: Object.keys(data)[index],
+            subcategory: [
+                ...data[key]
+            ],
+        }
+    )
+);
+
+const ExpandableComponent = ({item, onClickFunction}) =>{
+    const [layoutHeight, setlayoutHeight] = useState(0)
+
+    useEffect(() => {
+        if(item.isExpanded){
+            setlayoutHeight(null);
+        } else{
+            setlayoutHeight(0);
+        }
+        
+    }, [item.isExpanded])
+    
+    return (
+    <View>
+        <TouchableOpacity onPress={onClickFunction} style={styles.months}>
+            <Text>{item.category_name}</Text>
+            <Image style={[styles.caretIcon, { transform: 'rotateX(180deg)' }] } source={require("../assets/images/caret-down.svg")} />
+        </TouchableOpacity>
+        <View style={{overflow: "hidden", height: layoutHeight}}>
+            {
+                item.subcategory.map((item, key) => (
+                    <TouchableOpacity style={styles.dailyEntry}>
+                        <Text>
+                            {item.verse}
+                        </Text>
+                        <TouchableOpacity style={styles.check}></TouchableOpacity>
+                    </TouchableOpacity>
+                ))
+            }
+        </View>
+    </View>
+
+    );
+}
+
+export default function Brp({}) {
+
+    const [listData, setListData] = useState(content);
+    const updateLayout = (index) => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        const array = [...listData];
+        array.map((value, placeIndex)=>
+            placeIndex === index 
+            ? (array[placeIndex]['isExpanded']) = !array[placeIndex]['isExpanded']
+            : (array[placeIndex]['isExpanded']) = false
+        );
+        setListData(array);
+    }
+
     return (
     <View style={styles.container}>
         <TouchableOpacity style={styles.write}>
             <Text>Write your Journal Entry today now!</Text>
         </TouchableOpacity>
+
+        <ScrollView style={styles.contentContainer} showsHorizontalScrollIndicator={false}>
+            {
+                listData.map( (item, key) => (
+                    <ExpandableComponent 
+                        key={item.category_name}
+                        item={item}
+                        onClickFunction={()=>{
+                            updateLayout(key)
+                        }}
+                    />
+                ))
+            }
+        </ScrollView>
+        
     </View>
     )
 }
@@ -15,6 +92,8 @@ const styles = StyleSheet.create({
     container:{
         padding: 10,
         flex:1,
+        backgroundColor: "#fff",
+        gap: 10,
     },
     write:{
         height: 70,
@@ -23,5 +102,41 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "black",
         borderRadius: 5,
+    },
+    contentContainer:{
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 5,
+        padding: 10,
+    },
+    months:{
+        padding: 10,
+        height: 50,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        borderColor: "black",
+        borderBottomWidth: 1,
+    },
+    caretIcon:{
+        width: 25,
+        height: 25,
+
+    },
+    dailyEntry:{
+        padding: 20,
+        height: 50,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        borderColor: "black",
+        borderBottomWidth: 1,
+    },
+    check:{
+        width: 25,
+        height: 25,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "black",
     },
 })
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,21 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import NavButton from '../components/NavButton';
-
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+//for the icons
 const path = [require("../assets/images/brp.png"), require("../assets/images/mirror.png"), require("../assets/images/person.png"), require("../assets/images/info.png"), ];
+
+// date setting
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 const m = new Date().getMonth();
 const d = new Date().getDate();
 const today = `${month[m]} ${d}`
+
+//verses setting
+
+const data = require("../constants/data.json");
+
+const verse = data[month[m]][d-1]["verse"];
 
 
 
@@ -21,7 +29,7 @@ export default function Home({navigation}) {
     return (
         <View style={styles.container}>
             <View  style={styles.journal}>
-            <Text style={{fontFamily: "League-Spartan-M", fontSize:20}}>Journal 2023</Text>
+            <Text style={{fontFamily: "League-Spartan-M", fontSize:25}}>Journal 2023</Text>
 
             </View>
             
@@ -30,12 +38,16 @@ export default function Home({navigation}) {
                 <Text>{today}</Text>
             </View>
 
-            <TouchableOpacity style={styles.verse}>
-                <Text>Matthew 28:19-20</Text>
-                <Image source={require('../assets/images/arrow-right.png')}/>
+            <TouchableOpacity style={[styles.verse, styles.border]}>
+                <Text>{verse}</Text>
+                <Image style={{width: 25, height: 25}}source={require("../assets/images/arrow-right.png")}/>
             </TouchableOpacity>
 
-            <View style={styles.navContainer}>
+            <View style={[styles.recentNotes, styles.border]}>
+                <Text>Recent Entries</Text>
+            </View>
+
+            <View style={[styles.navContainer, styles.border]}>
                 < NavButton onPress={()=> {openBrp()} } path={path[0]} />
                 < NavButton path={path[1]} />
                 < NavButton path={path[2]} />
@@ -43,10 +55,6 @@ export default function Home({navigation}) {
 
             </View>
             
-            <View>
-                <Text>Add Entries</Text>
-            </View>
-
         </View>
     )
 }
@@ -56,11 +64,18 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         gap:10,
+        flexDirection: 'column',
+        backgroundColor: '#f2f2f2',
     },
     journal:{ 
         paddingTop: 20, 
         alignItems: "center",
         justifyContent:"center", 
+    },
+    border:{
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 5,
     },
 
     date:{
@@ -74,23 +89,23 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent:"space-between", 
         flexDirection: "row",
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 5,
+        backgroundColor: '#fff',
+    
     },
+    recentNotes:{
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+    
     navContainer:{
-        width:"100%",
-        position: "absolute",
-        left: 10,
-        bottom: 0,
+        height: 50,
         padding: 10,
         flexDirection: "row",
         justifyContent: 'center',
         alignItems: 'center',
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 5,
         gap: 20,
-        
+        backgroundColor: '#fff',
+    
     },
 })
